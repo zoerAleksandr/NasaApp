90a9f3556fdab3b7613f878c14c976f7ee9ed901
zoer <zoer.aleksandr@gmail.com>
2022-02-02 08:33:02 +0300

    Создал главный экран приложения с TextInputLayout, BottomSheet, BottomAppBar, FAB

--- app/src/main/java/com/example/nasaapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/nasaapp/ui/main/MainFragment.kt
@@ -2,11 +2,15 @@ package com.example.nasaapp.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.nasaapp.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainFragment : Fragment() {
 
@@ -15,6 +19,7 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +28,14 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
     }
 
+    private fun setBottomSheetBehavior(layout: FrameLayout){
+        bottomSheetBehavior = BottomSheetBehavior.from(layout)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
 }
